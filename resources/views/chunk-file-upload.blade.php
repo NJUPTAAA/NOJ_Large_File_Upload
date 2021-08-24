1610be75fa8b52add88c4bf7cfadeb72fb48a0d1
zsgsdesign <zsgsdesign@gmail.com>
2021-08-25 06:44:25 +0800

    modify style and locale

--- resources/views/chunk-file-upload.blade.php
+++ resources/views/chunk-file-upload.blade.php
@@ -9,13 +9,11 @@
 
         <div id="wrapper">
             <div id="container">
-                <!--头部，相册选择和格式选择-->
-
                 <div class="uploader" id="uploader{{$name}}">
                     <div class="queueList" id="queueList{{$name}}">
                         <div id="dndArea{{$name}}" class="placeholder">
                             <div id="filePicker{{$name}}"></div>
-                            <p>Or drag files here</p>
+                            <p>{{__('admin.chunkUpload.tooltip')}}</p>
                         </div>
                     </div>
                     <div class="statusBar" style="display:none;">
@@ -24,13 +22,13 @@
                             <span class="percentage"></span>
                         </div><div class="info"></div>
                         <div class="btns">
-                            <div class="filePicker2" id="filePicker2{{$name}}"></div><div class="uploadBtn">Start Uploading</div>
+                            <div class="filePicker2" id="filePicker2{{$name}}"></div><div class="uploadBtn">{{__('admin.chunkUpload.start')}}</div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-        <div class="input-group">
+        <div class="input-group" style="display: none;">
             <span class="input-group-addon"><i class="fa fa-pencil fa-fw"></i></span>
             <input readonly style="background-color: #fff" type="text" id="{{$name}}-savedpath" name="{{$name}}" value="{{ old($column, $value) }}" class="form-control title" placeholder="{{$label}}">
         </div>
